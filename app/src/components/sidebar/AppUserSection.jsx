7b0db332e364577904910cfa099de14b8f2bc422
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-01-29 14:24:33 +0100

    media section fix

--- app/src/components/sidebar/AppUserSection.jsx
+++ app/src/components/sidebar/AppUserSection.jsx
@@ -1,7 +1,19 @@
 function UserSection() {
     return (
         <>
-            <div id="user-section" className="flex-shrink-0">User</div>
+            <div id="user-section" className="flex-shrink-0 d-flex justify-content-between">
+                <div className="media">
+                    <figure>
+                        <img src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper-thumbnail.png" alt="Gabriele Lombardo" />
+                    </figure>
+                    <div className="h6 media-text">Gabriele Lombardo</div>
+                </div>
+                <div class="w-25 text-muted d-flex justify-content-around align-items-center">
+                    <i class="fa-solid fa-circle-notch" role="button"></i>
+                    <i class="fa-solid fa-comment-alt" role="button"></i>
+                    <i class="fa-solid fa-ellipsis-v" role="button"></i> 
+                </div>
+            </div>
         </>
     )
 }
