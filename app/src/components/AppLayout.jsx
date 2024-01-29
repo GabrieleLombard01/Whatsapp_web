3c9acfcb479bccc06e806c25756c074d14c712cc
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-01-29 12:40:47 +0100

    background

--- app/src/components/AppLayout.jsx
+++ app/src/components/AppLayout.jsx
@@ -3,7 +3,16 @@ function Layout() {
 
     return (
       <>
-        <h1>Hello world</h1>
+        <div className="h-100 d-flex align-items-center justify-content-center">
+          <main className="container shadow bg-white">
+            <div className="row h-100">
+            {/* SIDEBAR SECTION */}
+            <section id="sidebar" className="col-4 p-0 h-100 bg-light-gray"></section>
+            {/* CHAT SECTION */}
+            <section id="chat" className="col-8 p-0 h-100 bg-chat-color"></section>
+            </div>
+          </main>
+        </div>
       </>
     )
   }
