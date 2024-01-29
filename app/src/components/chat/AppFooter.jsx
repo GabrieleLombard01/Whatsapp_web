84975924eb757c4a8abcc37607877cb98f1da977
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-01-29 16:45:41 +0100

    messages completed

--- app/src/components/chat/AppFooter.jsx
+++ app/src/components/chat/AppFooter.jsx
@@ -2,7 +2,19 @@ function Footer() {
     return (
         <>
             {/* chat footer */}
-            <footer className="flex-shrink-0">footer</footer>
+            <footer className="flex-shrink-0 container-fluid pt-3 pb-3">
+                <div className="row h-100 align-items-center">
+                    <div className="col-1 d-flex justify-content-center">
+                        <i className="fa-regular fa-smile fa-2x text-muted" role="button"></i>
+                    </div>
+                    <div className="col-10">
+                        <input type="text" className="form-control" placeholder="Scrivi un messaggio" />
+                    </div>
+                    <div className="col-1 d-flex justify-content-center">
+                        <i className="fa-solid fa-microphone fa-2x text-muted" role="button"></i>
+                    </div>
+                </div>
+            </footer>
         </>
     )
 }
--- app/src/components/chat/AppMessages.jsx
+++ app/src/components/chat/AppMessages.jsx
@@ -2,7 +2,22 @@ function Messages() {
     return (
         <>
             {/* chat messages */}
-            <section className="flex-grow-1 overflow-auto bg-chat-image">messages</section>
+            <section className="flex-grow-1 overflow-auto bg-chat-image">
+                <ul className="px-3">
+                    <li className="message-container p-3 sent">
+                        <div className="message p-2 rounded-3">
+                            <p>Hai portato a spasso il cane?</p>
+                            <time dateTime="2024-01-03T09:53">29/01/2024 - 16:21</time>
+                        </div>
+                    </li>
+                    <li className="message-container p-3 received">
+                        <div className="message p-2 rounded-3">
+                            <p>Si!</p>
+                            <time dateTime="2024-01-03T09:53">29/01/2024 - 16:24</time>
+                        </div>
+                    </li>
+                </ul>
+            </section>
         </>
     )
 }
