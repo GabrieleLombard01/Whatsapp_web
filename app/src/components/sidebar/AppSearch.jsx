57f1ada676d5142d7b67fd34c2400d5a03a859b3
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-01-29 15:52:40 +0100

    search fixed

--- app/src/components/sidebar/AppSearch.jsx
+++ app/src/components/sidebar/AppSearch.jsx
@@ -1,7 +1,10 @@
 function Search() {
     return (
         <>
-            <div id="search-contacts" className="p-2 flex-shrink-0">Search</div>
+            <div id="search-contacts" className="p-2 flex-shrink-0">
+                <i className="fa-solid fa-search text-muted"></i>
+                <input type="text" className="w-100" placeholder="Cerca contatto"/>
+            </div>
         </>
     )
 }
