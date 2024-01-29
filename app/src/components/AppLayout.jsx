6047bbb70003d731f7af66213a0636f69772a379
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2024-01-29 13:34:04 +0100

    chat section

--- app/src/components/AppLayout.jsx
+++ app/src/components/AppLayout.jsx
@@ -3,13 +3,22 @@ function Layout() {
 
     return (
       <>
-        <div className="h-100 d-flex align-items-center justify-content-center">
+        <div id="content" className="h-100 d-flex align-items-center justify-content-center">
           <main className="container shadow bg-white">
             <div className="row h-100">
             {/* SIDEBAR SECTION */}
             <section id="sidebar" className="col-4 p-0 h-100 bg-light-gray"></section>
+
             {/* CHAT SECTION */}
-            <section id="chat" className="col-8 p-0 h-100 bg-chat-color"></section>
+            <section id="chat" className="col-8 p-0 h-100 bg-chat-color d-flex flex-column ">
+              {/* chat header */}
+              <header className="bg-light-gray flex-shrink-0">header</header>
+              {/* chat messages */}
+              <section className="flex-grow-1 overflow-auto bg-chat-image">messages</section>
+              {/* chat footer */}
+              <footer className="flex-shrink-0">footer</footer>
+            </section>
+
             </div>
           </main>
         </div>
